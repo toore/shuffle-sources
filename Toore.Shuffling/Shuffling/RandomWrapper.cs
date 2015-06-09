8d6148a1ab3c3cea01d2f637428c2fd2883aac92
dirge <andreas.torebring@gmail.com>
2015-06-09 22:57:42 +0200

    Clarified parameters names

--- Toore.Shuffling/Shuffling/RandomWrapper.cs
+++ Toore.Shuffling/Shuffling/RandomWrapper.cs
@@ -4,16 +4,16 @@ namespace Toore.Shuffling
 {
     public interface IRandomWrapper
     {
-        int Next(int minValue, int maxValue);
+        int Next(int minValueInclusive, int maxValueExclusive);
     }
 
     public class RandomWrapper : IRandomWrapper 
     {
         private readonly Random _random = new Random();
 
-        public int Next(int minValue, int maxValue)
+        public int Next(int minValueInclusive, int maxValueExclusive)
         {
-            return _random.Next(minValue, maxValue);
+            return _random.Next(minValueInclusive, maxValueExclusive);
         }
     }
 }
