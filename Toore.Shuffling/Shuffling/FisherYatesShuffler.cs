af196078906213f0c414324f6bc03404d2d02020
dirge <andreas.torebring@gmail.com>
2018-03-03 17:33:40 +0100

    Added documentation

--- Toore.Shuffling/Shuffling/FisherYatesShuffler.cs
+++ Toore.Shuffling/Shuffling/FisherYatesShuffler.cs
@@ -3,6 +3,13 @@ using System.Linq;
 
 namespace Toore.Shuffling
 {
+    // http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+    //
+    // To shuffle an array a of n elements (indices 0..n-1):
+    //  for i from 0 to n − 1 do
+    //       j ← random integer with i ≤ j < n
+    //       exchange a[j] and a[i]
+
     public class FisherYatesShuffler : IShuffler
     {
         private readonly IRandomWrapper _random;
@@ -12,6 +19,12 @@ namespace Toore.Shuffling
             _random = random;
         }
 
+        /// <summary>
+        /// Shuffles a collection of elements.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="elements">A collection of elements.</param>
+        /// <returns>A shuffled list.</returns>
         public List<T> Shuffle<T>(IEnumerable<T> elements)
         {
             var a = elements.ToList();
@@ -27,13 +40,6 @@ namespace Toore.Shuffling
             }
 
             return a;
-
-            // http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-
-            //To shuffle an array a of n elements (indices 0..n-1):
-            //  for i from 0 to n − 1 do
-            //       j ← random integer with i ≤ j < n
-            //       exchange a[j] and a[i]
         }
     }
 }
